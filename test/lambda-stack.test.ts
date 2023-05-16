f5139692a68aa2232b8c6855b474fd7b4470ebcd
Cheyo Codes <103604710+cheyocodes@users.noreply.github.com>
2023-05-16 14:07:23 -0500

    test: add lambda-stack test

---
+++ test/lambda-stack.test.ts
@@ -0,0 +1,10 @@
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { LambdaStack } from '../src/lambda-stack';
+
+test('Lambda created', () => {
+  const app = new cdk.App();
+  const stack = new LambdaStack(app, 'LambdaStack');
+  const template = Template.fromStack(stack);
+  template.resourceCountIs('AWS::Lambda::Function', 1);
+});
